1d8a8cf834b0ecd1f494a8b05b74db00366f70ce
Siddhant Maurya <siddhant.maurya.1122@gmail.com>
2023-06-07 14:47:38 +0530

    submitting code-Wed Jun 07 2023 14:47:38 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [name, setName] = useState("");
   return (
     <div>
         {/* Do not remove the main div */}
+        <form>
+          <label for="name">Enter your name:</label>
+          <br />
+          <input id="name" type="text" onChange={(e) => {setName(e.target.value)}} />
+        </form>
+        <p>{name &&  `Hello ${name}!`}</p>
     </div>
   )
+  
 }
 
-export default App
+
+export default App;
